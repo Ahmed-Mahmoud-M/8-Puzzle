d25db27f66d5bfc1e9cb74548a55f5ed7366c30c
Ahmed-Mahmoud-M <ahmedmahmoud842003@gmail.com>
2024-09-30 20:05:20 +0300

    searching strategy in search class (BFS,DFS,ASTAR)

---
+++ Search.py
@@ -0,0 +1,12 @@
+class Search:
+    def __init__(self,initalstate):
+        self.inital = initalstate
+
+
+    def BFS_search():
+        pass
+    def DFS_search():
+        pass
+
+    def AStar_search():
+        pass    
